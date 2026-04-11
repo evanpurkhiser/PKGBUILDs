57dd53bc31430328efc999267a8c770b6cce4380
Evan Purkhiser <evanpurkhiser@gmail.com>
2026-04-11 13:41:16 -0400

    Add AUR publish progress markers

--- ci/publish-aur.sh
+++ ci/publish-aur.sh
@@ -9,6 +9,8 @@ fi
 
 package="$1"
 
+echo "AUR publish start: ${package}"
+
 if [ -z "${PRIVATE_KEY_AUR:-}" ]; then
 	echo "PRIVATE_KEY_AUR is not set" >&2
 	exit 2
@@ -23,4 +25,6 @@ mkdir -p "$HOME/.ssh"
 echo "aur.archlinux.org ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLMiLrP8pVi5BFX2i3vepSUnpedeiewE5XptnUnau+ZoeUOPkpoCgZZuYfpaIQfhhJJI5qgnjJmr4hyJbe/zxow=" >>"$HOME/.ssh/known_hosts"
 
 /pkgs/aur-push "/pkgs/${package}"
+echo "AUR push completed: ${package}"
 git ls-remote "ssh+git://aur@aur.archlinux.org/${package}.git" refs/heads/master | grep -q .
+echo "AUR remote verified: ${package}"
